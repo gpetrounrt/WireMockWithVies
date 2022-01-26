d0aac7837fc5a38380bdf5308b2e56bb197fade8
Giorgos Petrou <g.petrou@enartia.com>
2022-01-26 12:43:08 +0200

    Add WireMockWithVies project and solution

---
+++ WireMockWithVies/WireMockWithViesTest.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using ViesService;
+using WireMock.Server;
+using WireMock.Settings;
+using Xunit;
+
+namespace WireMockWithVies
+{
+    public class WireMockWithViesTest
+    {
+        [Fact]
+        public async Task WireMockWithVies_ShouldReturnFalse()
+        {
+            var wireMockServerSettings = new WireMockServerSettings()
+            {
+                Urls = new[] { "http://localhost:15000/" },
+                StartAdminInterface = true,
+                ProxyAndRecordSettings = new ProxyAndRecordSettings
+                {
+                    Url = "http://ec.europa.eu/taxation_customs/vies/services/checkVatService",
+                    SaveMapping = true,
+                    SaveMappingToFile = true,
+                    AllowAutoRedirect = true
+                }
+            };
+
+            var wireMockServer = WireMockServer.Start(wireMockServerSettings);
+
+            checkVatPortTypeClient client = new checkVatPortTypeClient(checkVatPortTypeClient.EndpointConfiguration.checkVatPort, "http://ec.europa.eu/taxation_customs/vies/services/checkVatService");
+            checkVatRequest request = new("GR", "123456789");
+            var exception = await Record.ExceptionAsync(() => client.checkVatAsync(request));
+
+            wireMockServer.Stop();
+
+            Assert.Equal("INVALID_INPUT", exception.Message);
+        }
+    }
+}
